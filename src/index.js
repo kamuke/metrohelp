d7408824781baf374b31ba4c5ff5935d9a2e670c
ysi <catrina.koskinen@gmail.com>
2023-03-03 09:58:28 +0200

    fix weatherapi URL
    http -> https
    + slight jdoc fix

--- src/index.js
+++ src/index.js
@@ -284,7 +284,7 @@ const getWeather = async (selectedCampus, allCampuses) => {
     if (selectedCampus === campus.name) {
       try {
         //start fetch
-        const response = await fetch('http://api.weatherapi.com/v1/forecast.json?key=70ce88e5c2634487b5675944232702&q='+campus.city+'&days=1&aqi=no&alerts=no');
+        const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=70ce88e5c2634487b5675944232702&q='+campus.city+'&days=1&aqi=no&alerts=no');
         //If error
         if (!response.ok) throw new Error('Something went wrong.');
         const weather = await response.json();
@@ -301,8 +301,7 @@ const getWeather = async (selectedCampus, allCampuses) => {
  * Render current weather
  *
  * @author Catrina
- * @param weather
- * @returns {Promise<void>}
+ * @param weather - current weather
  */
 const renderWeather = async(weather) => {
   const weatherImg = document.querySelector('#weather-img');
